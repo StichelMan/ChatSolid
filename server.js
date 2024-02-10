ef6dc40667b64d7e6fb5139728ae3797cfd98455
Eli Van Stichelen <114239211+elivanstichelen@users.noreply.github.com>
2024-02-10 02:28:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const https = require("https");
 const app = express();
 const server = https.createServer(app);
 const socket = require("socket.io");
-const io = socket(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+const io = socket(server);
+
+// Set up CORS headers for all routes
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
 
 const users = {};
